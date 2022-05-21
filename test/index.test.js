3c785b0d4ae599883ad047217f6e9b1fc1289816
Evan Tran <evant179@gmail.com>
2022-05-21 16:39:08 -0700

    remove sample test

--- test/index.test.js
+++ test/index.test.js
@@ -2,8 +2,3 @@ test('this is a placeholder test', () => {
   console.log('hello, i am running this test!!');
   expect(true).toBe(true);
 });
-
-test('test workflow with failing test', () => {
-  console.log('this test will fail');
-  expect(true).toBe(false);
-});
