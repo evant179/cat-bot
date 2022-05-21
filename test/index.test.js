2fa167870e6c6aec9f5e71294e88855bddc6f573
Evan Tran <evant179@gmail.com>
2022-05-21 16:34:59 -0700

    test failing unit test

--- test/index.test.js
+++ test/index.test.js
@@ -2,3 +2,8 @@ test('this is a placeholder test', () => {
   console.log('hello, i am running this test!!');
   expect(true).toBe(true);
 });
+
+test('test workflow with failing test', () => {
+  console.log('this test will fail');
+  expect(true).toBe(false);
+});
