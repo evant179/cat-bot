94b6315ccf7f32a79bf76d127a3ec980db30a6a1
Evan Tran <evant179@gmail.com>
2022-05-31 10:54:37 -0700

    cleanup

--- src/twitter.js
+++ src/twitter.js
@@ -1,93 +1,30 @@
-// const got = require('got');
 const axios = require('axios').default;
 const crypto = require('crypto');
 const OAuth = require('oauth-1.0a');
-const qs = require('querystring');
 
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const {
+  // credentials for the app
+  TWITTER_CONSUMER_KEY,
+  TWITTER_CONSUMER_SECRET,
+  // credentials for the user (aka client)
+  TWITTER_CLIENT_ID,
+  TWITTER_CLIENT_SECRET,
+} = process.env;
 
-
-// The code below sets the consumer key and consumer secret from your environment variables
-// To set environment variables on macOS or Linux, run the export commands below from the terminal:
-// export CONSUMER_KEY='YOUR-KEY'
-// export CONSUMER_SECRET='YOUR-SECRET'
-const consumer_key = process.env.TWITTER_CONSUMER_KEY;
-const consumer_secret = process.env.TWITTER_CONSUMER_SECRET;
-
-
-// Be sure to add replace the text of the with the text you wish to Tweet.
-// You can also add parameters to post polls, quote Tweets, Tweet with reply settings, and Tweet to Super Followers in addition to other features.
 const data = {
   'text': `test!!! ${Date.now()}`
 };
 
 const endpointURL = `https://api.twitter.com/2/tweets`;
 
-// this example uses PIN-based OAuth to authorize the user
-const requestTokenURL = 'https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write';
-const authorizeURL = new URL('https://api.twitter.com/oauth/authorize');
-const accessTokenURL = 'https://api.twitter.com/oauth/access_token';
 const oauth = OAuth({
   consumer: {
-    key: consumer_key,
-    secret: consumer_secret
+    key: TWITTER_CONSUMER_KEY,
+    secret: TWITTER_CONSUMER_SECRET
   },
   signature_method: 'HMAC-SHA1',
   hash_function: (baseString, key) => crypto.createHmac('sha1', key).update(baseString).digest('base64')
 });
-
-async function input(prompt) {
-  return new Promise(async (resolve, reject) => {
-    readline.question(prompt, (out) => {
-      readline.close();
-      resolve(out);
-    });
-  });
-}
-
-async function requestToken() {
-  const authHeader = oauth.toHeader(oauth.authorize({
-    url: requestTokenURL,
-    method: 'POST'
-  }));
-
-  const req = await axios.post(requestTokenURL, {}, {
-    headers: {
-      Authorization: authHeader["Authorization"]
-    }
-  });
-  if (req.data) {
-    return qs.parse(req.data);
-  } else {
-    throw new Error('Cannot get an OAuth request token');
-  }
-}
-
-
-async function accessToken({
-  oauth_token,
-  oauth_token_secret
-}, verifier) {
-  const authHeader = oauth.toHeader(oauth.authorize({
-    url: accessTokenURL,
-    method: 'POST'
-  }));
-  const path = `https://api.twitter.com/oauth/access_token?oauth_verifier=${verifier}&oauth_token=${oauth_token}`
-  const req = await axios.post(path, {}, {
-    headers: {
-      Authorization: authHeader["Authorization"]
-    }
-  });
-  if (req.data) {
-    return qs.parse(req.data);
-  } else {
-    throw new Error('Cannot get an OAuth request token');
-  }
-}
-
 
 async function getRequest({
   oauth_token,
@@ -122,20 +59,9 @@ async function getRequest({
 
 (async () => {
   try {
-    // // Get request token
-    // const oAuthRequestToken = await requestToken();
-    // // Get authorization
-    // authorizeURL.searchParams.append('oauth_token', oAuthRequestToken.oauth_token);
-    // console.log('Please go here and authorize:', authorizeURL.href);
-    // const pin = await input('Paste the PIN here: ');
-    // // Get the access token
-    // const oAuthAccessToken = await accessToken(oAuthRequestToken, pin.trim());
-    // // Make the request
-    // console.log('--- oAuthAccessToken: ', oAuthAccessToken);
-    // const response = await getRequest(oAuthAccessToken);
     const response = await getRequest({
-      oauth_token: process.env.TWITTER_CLIENT_ID,
-      oauth_token_secret: process.env.TWITTER_CLIENT_SECRET,
+      oauth_token: TWITTER_CLIENT_ID,
+      oauth_token_secret: TWITTER_CLIENT_SECRET,
     });
     console.dir(response, {
       depth: null
