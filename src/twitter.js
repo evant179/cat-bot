c94a342e73880296ba24af52d3f69cca44ba8f29
Evan Tran <evant179@gmail.com>
2022-05-31 18:38:24 -0700

    lint

--- src/twitter.js
+++ src/twitter.js
@@ -15,10 +15,10 @@ const createAuthHeader = (url) => {
   const oauth = OAuth({
     consumer: {
       key: TWITTER_CONSUMER_KEY,
-      secret: TWITTER_CONSUMER_SECRET
+      secret: TWITTER_CONSUMER_SECRET,
     },
     signature_method: 'HMAC-SHA1',
-    hash_function: (baseString, key) => crypto.createHmac('sha1', key).update(baseString).digest('base64')
+    hash_function: (baseString, key) => crypto.createHmac('sha1', key).update(baseString).digest('base64'),
   });
 
   const token = {
@@ -26,40 +26,40 @@ const createAuthHeader = (url) => {
     secret: TWITTER_CLIENT_SECRET,
   };
 
-  return oauth.toHeader(oauth.authorize({
+  const { Authorization: authHeader } = oauth.toHeader(oauth.authorize({
     url,
-    method: 'POST'
+    method: 'POST',
   }, token));
-}
 
-const createBody = () => {
-  return {
-    'text': `test!!! ${Date.now()}`
-  };
+  return authHeader;
 };
 
+const createBody = () => ({
+  text: `test!!! ${Date.now()}`,
+});
+
 const createTweet = async () => {
-  const url = `https://api.twitter.com/2/tweets`;
+  const url = 'https://api.twitter.com/2/tweets';
   const authHeader = createAuthHeader(url);
   const body = createBody();
 
   const response = await axios.post(url, body, {
     headers: {
-      Authorization: authHeader["Authorization"],
-      'user-agent': "v2CreateTweetJS",
-      'content-type': "application/json",
-      'accept': "application/json"
-    }
+      Authorization: authHeader,
+      'user-agent': 'v2CreateTweetJS',
+      'content-type': 'application/json',
+      accept: 'application/json',
+    },
   });
 
-  if (response.data) {
-    console.log('Successful tweet!', JSON.stringify(response.data, null, 2));
-    return response.data;
-  } else {
+  if (!response.data) {
     // TODO - parse useful error message from response
     throw new Error('Unsuccessful request');
   }
-}
+
+  console.log('Successful tweet!', JSON.stringify(response.data, null, 2));
+  return response.data;
+};
 
 module.exports = {
   createTweet,
