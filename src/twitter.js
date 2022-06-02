da5c67424610fd0e6feeeaf47859239797cc5c93
Evan Tran <evant179@gmail.com>
2022-06-01 19:39:14 -0700

    Twitter upload media (#11)
    * add test image
    
    * upload image to twitter and tweet as media

--- src/twitter.js
+++ src/twitter.js
@@ -11,16 +11,16 @@ const {
   TWITTER_CLIENT_SECRET,
 } = process.env;
 
-const createAuthHeader = (url) => {
-  const oauth = OAuth({
-    consumer: {
-      key: TWITTER_CONSUMER_KEY,
-      secret: TWITTER_CONSUMER_SECRET,
-    },
-    signature_method: 'HMAC-SHA1',
-    hash_function: (baseString, key) => crypto.createHmac('sha1', key).update(baseString).digest('base64'),
-  });
+const oauth = OAuth({
+  consumer: {
+    key: TWITTER_CONSUMER_KEY,
+    secret: TWITTER_CONSUMER_SECRET,
+  },
+  signature_method: 'HMAC-SHA1',
+  hash_function: (baseString, key) => crypto.createHmac('sha1', key).update(baseString).digest('base64'),
+});
 
+const createAuthHeader = (url) => {
   const token = {
     key: TWITTER_CLIENT_ID,
     secret: TWITTER_CLIENT_SECRET,
@@ -34,14 +34,47 @@ const createAuthHeader = (url) => {
   return authHeader;
 };
 
-const createBody = () => ({
-  text: `test!!! ${Date.now()}`,
-});
+const uploadImage = async (encodedImage) => {
+  const url = 'https://upload.twitter.com/1.1/media/upload.json';
+  const authHeader = createAuthHeader(url);
+  const body = {
+    media_data: encodedImage,
+  };
 
-const createTweet = async () => {
+  /**
+   * From: https://developer.twitter.com/en/docs/twitter-api/v1/media/upload-media/api-reference/post-media-upload
+   *
+   * Ensure the POST is a multipart/form-data request. Either upload the raw
+   * binary (media parameter) of the file, or its base64-encoded contents
+   * (media_data parameter). Use raw binary when possible, because base64
+   * encoding results in larger file sizes
+   */
+  const response = await axios.post(url, body, {
+    headers: {
+      Authorization: authHeader,
+      'content-type': 'multipart/form-data',
+      accept: 'application/json',
+    },
+  });
+
+  if (!response.data) {
+    // TODO - parse useful error message from response
+    throw new Error('Unsuccessful request');
+  }
+
+  console.log('Successful image upload!', JSON.stringify(response.data, null, 2));
+  return response.data.media_id_string;
+};
+
+const createTweet = async (mediaId) => {
   const url = 'https://api.twitter.com/2/tweets';
   const authHeader = createAuthHeader(url);
-  const body = createBody();
+  const body = {
+    text: `test with image! ${Date.now()}`,
+    media: {
+      media_ids: [mediaId],
+    },
+  };
 
   const response = await axios.post(url, body, {
     headers: {
@@ -63,4 +96,5 @@ const createTweet = async () => {
 
 module.exports = {
   createTweet,
+  uploadImage,
 };
