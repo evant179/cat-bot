9e1523dc1f5f92ae565d523f14bf06b6147b5c92
Evan Tran <evant179@gmail.com>
2022-06-04 16:45:32 -0700

    read image files from s3

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 const fs = require('fs');
+const { listObjects } = require('./s3');
 const { createTweet, uploadImage } = require('./twitter');
-// const AWS = require('aws-sdk');
-// const s3 = new AWS.S3();
+
+const {
+  // feature flags for local development
+  IS_POSTING_TWEET = 'true',
+} = process.env;
+
+const isPostingTweet = () => (IS_POSTING_TWEET === 'true');
 
 const handler = async (event) => {
   console.log(event);
 
   try {
+    await listObjects();
+
     // TODO - reading local image for now
     const encodedImage = fs.readFileSync('test/data/test-image.png').toString('base64');
-    const mediaId = await uploadImage(encodedImage);
-    await createTweet(mediaId);
+
+    if (isPostingTweet()) {
+      const mediaId = await uploadImage(encodedImage);
+      await createTweet(mediaId);
+    } else {
+      console.log('Skip tweeting -- IS_POSTING_TWEET:', IS_POSTING_TWEET);
+    }
   } catch (e) {
     const { response = {} } = e;
     const { data } = response;
