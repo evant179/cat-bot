b90a79777c77f956fc817b8298c7a6bda29cf5e4
Evan Tran <evant179@gmail.com>
2022-06-05 09:36:14 -0700

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-const { getObject, listObjects } = require('./s3');
-const { createTweet, uploadImage } = require('./twitter');
+const s3 = require('./s3');
+const twitter = require('./twitter');
 
 const {
   // feature flags for local development
@@ -12,9 +12,7 @@ const isPostingTweet = () => (IS_POSTING_TWEET === 'true');
 const getRandomItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 const handler = async (event) => {
-  console.log(event);
-
-  const objects = await listObjects('staging/');
+  const objects = await s3.listObjects('staging/');
   if (!objects.length) {
     // TODO - reset folder state:
     //   1. copy all objects from "tweeted" folder to "staging" folder
@@ -25,21 +23,21 @@ const handler = async (event) => {
 
   const { Key: key } = getRandomItem(objects);
   console.log('Attempt to retrieve from s3 -- key:', key);
-  const buffer = await getObject(key);
+  const buffer = await s3.getObject(key);
   console.log('Attempt to encode image -- key:', key);
   const encodedImage = buffer.toString('base64');
 
   try {
     if (isPostingTweet()) {
-      const mediaId = await uploadImage(encodedImage);
-      await createTweet(mediaId);
+      const mediaId = await twitter.uploadImage(encodedImage);
+      await twitter.createTweet(mediaId);
     } else {
       console.log('Skip tweeting -- IS_POSTING_TWEET:', IS_POSTING_TWEET);
     }
   } catch (e) {
     const { response = {} } = e;
     const { data } = response;
-    console.error('Error details:', data);
+    console.error('Twitter error details:', data);
     throw e;
   }
 
