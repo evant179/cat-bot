5c882f1c245813a3d2a771d1633d3e3b34142245
Evan Tran <evant179@gmail.com>
2022-06-04 17:45:24 -0700

    rename env vars to not conflict with lambda's reserved env vars

--- src/s3.js
+++ src/s3.js
@@ -1,19 +1,19 @@
 const AWS = require('aws-sdk');
 
 const {
+  S3_BUCKET_NAME,
   // credentials for aws-sdk usage:
   //   - if running locally, these will set
   //   - otherwise, when running within lambda, these won't be set
   //       and permission is gained from its IAM role
-  AWS_ACCESS_KEY_ID,
-  AWS_SECRET_ACCESS_KEY,
-  S3_BUCKET_NAME,
+  LOCAL_AWS_ACCESS_KEY_ID,
+  LOCAL_AWS_SECRET_ACCESS_KEY,
 } = process.env;
 
 const createS3Client = () => {
-  const awsCredentials = AWS_ACCESS_KEY_ID ? {
-    accessKeyId: AWS_ACCESS_KEY_ID,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  const awsCredentials = LOCAL_AWS_ACCESS_KEY_ID ? {
+    accessKeyId: LOCAL_AWS_ACCESS_KEY_ID,
+    secretAccessKey: LOCAL_AWS_SECRET_ACCESS_KEY,
   } : {};
 
   return new AWS.S3({
