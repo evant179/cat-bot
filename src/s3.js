ba0b9600db4d9d3b520666ecbf8e68399e7a6ae7
Evan Tran <evant179@gmail.com>
2022-06-04 19:46:07 -0700

    Tweet image from s3 (#17)
    * list images from a specific folder
    
    * encode image from s3; tweet encoded image
    
    * increase lambda timeout

--- src/s3.js
+++ src/s3.js
@@ -25,12 +25,28 @@ const listObjects = async (prefix) => {
   const s3 = createS3Client();
   const params = {
     Bucket: S3_BUCKET_NAME,
-    // Prefix: 'STRING_VALUE',
+    Prefix: prefix,
+    // start after the prefix so the response won't
+    //   include the "folder" object key
+    StartAfter: prefix,
   };
   const result = await s3.listObjectsV2(params).promise();
   console.log('listObjectsV2:', JSON.stringify(result, null, 2));
+  const { Contents: objects } = result;
+  return objects;
+};
+
+const getObject = async (key) => {
+  const s3 = createS3Client();
+  const params = {
+    Bucket: S3_BUCKET_NAME,
+    Key: key,
+  };
+  const { Body: buffer } = await s3.getObject(params).promise();
+  return buffer;
 };
 
 module.exports = {
+  getObject,
   listObjects,
 };
