f74e55cf37e8214421671f78b8a243f1ecc6f20e
neato0079 <neato0079@gmail.com>
2022-06-18 23:40:47 -0700

    Add functionality to read all files of uploads folder

--- src/utils/send-it.js
+++ src/utils/send-it.js
@@ -2,5 +2,20 @@
 // navigate to S3 bucket
 // select images to upload
 // confirm images to upload
+const fs = require('fs');
 
-console.log('Does this work') // test npm full send
+const uploadsFolder = 'uploads/';
+
+fs.readdir(uploadsFolder, (err, files) => {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach( file => {
+        // Do whatever you want to do with the file
+        console.log(file); 
+    });
+    console.log('Done') // npm run full-send
+});
+
