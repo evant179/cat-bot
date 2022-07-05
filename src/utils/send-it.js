f6c20f4cef0519ac09281267eb084283faa77c84
Matt Quiba <neato0079@gmail.com>
2022-07-05 07:17:54 -0700

    Test with npm args

--- src/utils/send-it.js
+++ src/utils/send-it.js
@@ -6,7 +6,11 @@ const uploadsFolder = 'uploads/';
 const folderContents = fs.readdirSync(uploadsFolder);
 const targetFolder = process.argv.slice(2)[0];
 
-s3.uploadFolderContents(folderContents, uploadsFolder, targetFolder);
+if (targetFolder !== 'test-quarantine/') {
+  console.log('script did not take "test-quarantine param"\nExiting program');
+} else {
+  s3.uploadFolderContents(folderContents, uploadsFolder, targetFolder);
+}
 
 // resources:
 // https://stackabuse.com/uploading-files-to-aws-s3-with-node-js/
@@ -18,3 +22,8 @@ s3.uploadFolderContents(folderContents, uploadsFolder, targetFolder);
 // const folderContent = fs.readdirSync(args);
 
 // console.log(folderContent);
+
+// https://stackoverflow.com/questions/11580961/sending-command-line-arguments-to-npm-script
+
+// syntax for passing arg through npm: npm run <command> [-- <args>]
+// example: npm run full-send test-quarantine/ will pass 'test-quarantine/' as an arg
