b32fd5e36c4341fb760eb14b632bf4cde4107f00
Evan Tran <evant179@gmail.com>
2022-07-17 18:06:22 -0700

    Prevent uploading duplicate images when using the upload utility script (#35)
    * Make listObjects more robust by handling NextContinuationToken (this will occur if there are >1000 objects in a given folder)
    
    * Add listRawFileNames to s3.js; add corresponding sample response data
    
    * Deduplicate files before uploading to s3
    
    * Add comment

--- src/utils/send-it.js
+++ src/utils/send-it.js
@@ -2,10 +2,38 @@ const fs = require('fs');
 const s3 = require('../s3');
 
 const uploadsFolder = 'uploads/';
-const folderContents = fs.readdirSync(uploadsFolder);
-const targetFolder = 'test-staging/';
+const targetFolder = 'staging/';
 
-s3.uploadFolderContents(folderContents, uploadsFolder, targetFolder);
+const deduplicate = (localRawFileNames, existingRawFileNames) => {
+  const uniqueRawFileNames = localRawFileNames
+    .filter((fileName) => !existingRawFileNames.includes(fileName));
+
+  console.log('After deduplication, number of unique local files: ', uniqueRawFileNames.length);
+  return uniqueRawFileNames;
+};
+
+const main = async () => {
+  const localRawFileNames = fs.readdirSync(uploadsFolder);
+  console.log(`Total number of files found from local folder [${uploadsFolder}]: ${localRawFileNames.length}`);
+
+  // TODO - Simply grabbing both the live and test folders for now.
+  // There is a WIP feature that will change this to be driven by user input.
+  const existingRawFileNames = await s3.listRawFileNames([
+    'staging/',
+    'tweeted/',
+    'quarantine/',
+    'test-staging/',
+    'test-tweeted/',
+    'test-quarantine/',
+  ]);
+  const uniqueLocalRawFileNames = deduplicate(localRawFileNames, existingRawFileNames);
+
+  await s3.uploadFolderContents(uniqueLocalRawFileNames, uploadsFolder, targetFolder);
+};
+
+main()
+  .then(() => console.log(`Done uploading files to: ${targetFolder}`))
+  .catch((e) => console.error(`Error while uploading files to: ${targetFolder} `, e));
 
 // resources:
 // https://stackabuse.com/uploading-files-to-aws-s3-with-node-js/
