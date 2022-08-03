293680957d8c4d3943d4010822b204dcc8d696ec
Evan Tran <evant179@gmail.com>
2022-08-03 03:57:55 -0700

    Update s3.listObjects unit tests (#40)

--- test/unit/services/s3.test.js
+++ test/unit/services/s3.test.js
@@ -7,6 +7,7 @@ afterEach(() => {
 describe('listObjects', () => {
   test('lists objects from s3 exactly once', async () => {
     const baseResponse = require('../../data/s3-response-list-objects-v2.json');
+    const testFolder = 'my-test-folder/';
 
     // setup mocks
     const listObjectsV2 = jest.fn().mockReturnValue({
@@ -17,77 +18,65 @@ describe('listObjects', () => {
     };
 
     // test
-    const result = await listObjects('my-test-folder/', s3);
+    const result = await listObjects(testFolder, s3);
 
     // assert
     expect(listObjectsV2).toHaveBeenCalledTimes(1);
+    expect(listObjectsV2).toHaveBeenCalledWith(
+      expect.objectContaining({
+        Prefix: testFolder,
+        StartAfter: testFolder,
+        ContinuationToken: undefined,
+      }),
+    );
     expect(result).toEqual(baseResponse.Contents);
   });
+
+  test('lists objects from s3 with multiple calls using NextContinuationToken / ContinuationToken', async () => {
+    const baseResponse = require('../../data/s3-response-list-objects-v2.json');
+    const testFolder = 'my-test-folder/';
+    const testNextToken = 'my-test-next-token';
+
+    // setup mocks
+    const listObjectsV2 = jest.fn()
+      .mockReturnValueOnce({
+        promise: () => Promise.resolve({
+          ...baseResponse,
+          NextContinuationToken: testNextToken,
+        }),
+      })
+      .mockReturnValueOnce({
+        promise: () => Promise.resolve(baseResponse),
+      });
+    const s3 = {
+      listObjectsV2,
+    };
+
+    // test
+    const result = await listObjects(testFolder, s3);
+
+    // assert
+    expect(listObjectsV2).toHaveBeenCalledTimes(2);
+    expect(listObjectsV2).toHaveBeenNthCalledWith(
+      1,
+      expect.objectContaining({
+        Prefix: testFolder,
+        StartAfter: testFolder,
+        ContinuationToken: undefined,
+      }),
+    );
+    expect(listObjectsV2).toHaveBeenNthCalledWith(
+      2,
+      expect.objectContaining({
+        Prefix: testFolder,
+        StartAfter: testFolder,
+        ContinuationToken: testNextToken,
+      }),
+    );
+    expect(result).toEqual([
+      // baseResponse.Contents was returned twice
+      ...baseResponse.Contents,
+      ...baseResponse.Contents,
+    ]);
+  });
 });
-
-// const { retryBatchWriteItem } = require('./utils');
-
-// describe('#retryBatchWriteItem tests', () => {
-//   it('should handle empty UnprocessedItems result', async () => {
-//     const mockFn = jest.fn().mockResolvedValueOnce({
-//       UnprocessedItems: {},
-//     });
-//     const fnParams = {
-//       RequestItems: ['i-am-test-01'],
-//     };
-
-//     const result = await retryBatchWriteItem(mockFn, fnParams);
-
-//     expect(mockFn).toHaveBeenCalledTimes(1);
-//     expect(mockFn.mock.calls[0][0]).toEqual(fnParams);
-//     expect(result).toEqual({
-//       UnprocessedItems: {},
-//     });
-//   });
-
-//   it('should handle non-empty UnprocessedItems result and retry once, then succeed', async () => {
-//     const mockFn = jest.fn()
-//       .mockResolvedValueOnce({
-//         UnprocessedItems: ['retry-me-01'],
-//       })
-//       .mockResolvedValueOnce({
-//         UnprocessedItems: {},
-//       });
-//     const fnParams = {
-//       RequestItems: ['i-am-test-01'],
-//     };
-
-//     const result = await retryBatchWriteItem(mockFn, fnParams);
-
-//     expect(mockFn).toHaveBeenCalledTimes(2);
-//     expect(mockFn.mock.calls[0][0]).toEqual(fnParams);
-//     expect(mockFn.mock.calls[1][0]).toEqual({
-//       RequestItems: ['retry-me-01'],
-//     });
-//     expect(result).toEqual({
-//       UnprocessedItems: {},
-//     });
-//   });
-
-//   it('should exceed maximum retries and throw', async () => {
-//     const mockFn = jest.fn()
-//       .mockResolvedValueOnce({
-//         UnprocessedItems: ['retry-me-01'],
-//       })
-//       .mockResolvedValueOnce({
-//         UnprocessedItems: ['retry-me-02'],
-//       });
-//     const fnParams = {
-//       RequestItems: ['i-am-test-01'],
-//     };
-//     const maxRetries = 2;
-
-//     expect.assertions(2); // expect error
-//     try {
-//       await retryBatchWriteItem(mockFn, fnParams, maxRetries);
-//     } catch (e) {
-//       expect(mockFn).toHaveBeenCalledTimes(2);
-//       expect(e).toContain('Maximum retries exceeded');
-//     }
-//   });
-// });
