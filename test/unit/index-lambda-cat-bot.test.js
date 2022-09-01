821309bf03a508fa9ae73f7e92a0fc45c13f8050
Matt Quiba <neato0079@gmail.com>
2022-09-01 11:44:12 -0700

    Begin writing unit test!

--- test/unit/index-lambda-cat-bot.test.js
+++ test/unit/index-lambda-cat-bot.test.js
@@ -1,4 +1,11 @@
+const { handler } = require('../../src/index-lambda-cat-bot');
+
 test('this is a placeholder test', () => {
   console.log('hello, i am running this test!!');
   expect(true).toBe(true);
 });
+
+
+test('handler exists', () => {
+  expect(handler()).toBeDefined()
+})
