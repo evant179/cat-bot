58f7e125ab5a8f29098e06249b4b4a162c6f7099
neato0079 <neato0079@gmail.com>
2022-09-06 04:28:35 -0700

    WIP empty staging folder reaction test

--- test/unit/index-lambda-cat-bot.test.js
+++ test/unit/index-lambda-cat-bot.test.js
@@ -1,4 +1,5 @@
 const { handler } = require('../../src/index-lambda-cat-bot');
+const s3 = require('../../src/services/s3');
 
 test('this is a placeholder test', () => {
   console.log('hello, i am running this test!!');
@@ -8,4 +9,27 @@ test('this is a placeholder test', () => {
 
 test('handler exists', () => {
   expect(handler()).toBeDefined()
+})
+
+describe('Empty staging folder reaction', () => {
+  test('Staging folder gets repopulated if it is empty', async() => {
+    // setup mocks
+    // mock listObjects so that it returns an empty array
+
+    // const spy = await jest.spyOn(s3, 'listObjects');
+    // spy.mockReturnValue([]);
+
+    s3.listObjects = await jest.fn().mockReturnValue([])
+
+    // test
+    // call handler
+    const test = async() => await handler()
+
+    // assert 
+    // expect a error message will be returned
+    // error msg: `Successfully repopulated ${STAGING_FOLDER} folder with ${TWEETED_FOLDER} contents. Proceed to rerun Lambda.`
+    expect(test).toThrow();
+
+    // spy.mockRestore();
+  })
 })
