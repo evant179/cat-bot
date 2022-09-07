cc03ed02c47e998c34ac3eb5bf653b88daaca19c
neato0079 <neato0079@gmail.com>
2022-09-06 22:23:45 -0700

    Successful test for staging folder repopulation logic

--- test/unit/index-lambda-cat-bot.test.js
+++ test/unit/index-lambda-cat-bot.test.js
@@ -1,5 +1,13 @@
 const { handler } = require('../../src/index-lambda-cat-bot');
 const s3 = require('../../src/services/s3');
+const twitter = require('../../src/services/twitter');
+
+jest.mock('s3');
+jest.mock('twitter')
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
 
 test('this is a placeholder test', () => {
   console.log('hello, i am running this test!!');
@@ -19,17 +27,21 @@ describe('Empty staging folder reaction', () => {
     // const spy = await jest.spyOn(s3, 'listObjects');
     // spy.mockReturnValue([]);
 
-    s3.listObjects = await jest.fn().mockReturnValue([])
+    s3.listObjects = await jest.fn().mockResolvedValue([])
+    // s3.listObjects.mockResolvedValue([])
 
     // test
     // call handler
-    const test = async() => await handler()
+    // const test = async() => await handler()
 
     // assert 
     // expect a error message will be returned
     // error msg: `Successfully repopulated ${STAGING_FOLDER} folder with ${TWEETED_FOLDER} contents. Proceed to rerun Lambda.`
-    expect(test).toThrow();
+    await expect(handler()).rejects.toThrow('Successfully repopulated');
 
     // spy.mockRestore();
   })
 })
+
+// TOTEST:
+// 
