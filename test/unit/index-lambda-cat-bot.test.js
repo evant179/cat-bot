d68e0a5196a9ddf7f716f5f995a576ecad5f4065
neato0079 <92897408+neato0079@users.noreply.github.com>
2022-09-13 07:03:51 -0700

    Catbot index unit test wip (#45)
    * Ignore local notes
    
    * Begin writing unit test!
    
    * WIP empty staging folder reaction test
    
    * Add module paths for jest
    
    * Successful test for staging folder repopulation logic
    
    * WIP unit testing
    
    * Successful testing of main tweeting function
    
    * Successful test of Twitter API error handling
    
    * Add assertions 'toHaveBeenCalledWith' for more robustnesss
    
    * Clean up comments and formatting
    
    * Use consistent require format
    
    * Consolidate 'describe' stuff

--- test/unit/index-lambda-cat-bot.test.js
+++ test/unit/index-lambda-cat-bot.test.js
@@ -1,4 +1,61 @@
-test('this is a placeholder test', () => {
-  console.log('hello, i am running this test!!');
-  expect(true).toBe(true);
+const index = require('../../src/index-lambda-cat-bot');
+const s3 = require('../../src/services/s3');
+const twitter = require('../../src/services/twitter');
+const fakeImages = require('../data/s3-response-list-objects-v2.json').Contents;
+const uploadImageAPIResponse = require('../data/twitter-response-upload-image.json');
+
+jest.mock('s3');
+jest.mock('twitter');
+
+afterEach(() => {
+  jest.resetAllMocks();
 });
+
+describe('handler', () => {
+  
+  test('twitter.uploadImage and twitter.createTweet are called if isTweetingEnabled is true', async () => {
+    // setup mocks so that images are detected in the staging folder
+    index.isTweetingEnabled = jest.fn().mockReturnValue(true);
+    s3.getObject = await jest.fn().mockResolvedValue('1');
+    s3.listObjects = await jest.fn().mockResolvedValue(fakeImages);
+    twitter.uploadImage = await jest.fn().mockResolvedValue(uploadImageAPIResponse)
+    
+    // test
+    const test = await index.handler();
+    
+    // assert
+    expect.assertions(4)
+    expect(twitter.uploadImage).toHaveBeenCalled();
+    expect(twitter.uploadImage).toHaveBeenCalledWith(expect.stringContaining(''));
+    expect(twitter.createTweet).toHaveBeenCalled();
+    expect(twitter.createTweet).toHaveBeenCalledWith(expect.objectContaining({ media_id: 1533168952922108000 }));
+  })
+  
+  test('Staging folder gets repopulated if it is empty', async () => {
+    // setup mocks
+    // mock empty staging folder
+    s3.listObjects = await jest.fn().mockResolvedValue([]);
+
+    // test and assert
+    expect.assertions(1)
+    await expect(index.handler).rejects.toThrow('Successfully repopulated');
+
+  })
+  
+  test('Error from twitter calls handled', async () => {
+    // mock twitter API error
+    twitter.createTweet = jest.fn().mockRejectedValue(new Error('Twitter API is borked'));
+
+    // setup mocks so that images are detected in the staging folder
+    index.isTweetingEnabled = jest.fn().mockReturnValue(true);
+    s3.getObject = await jest.fn().mockResolvedValue('1');
+    s3.listObjects = await jest.fn().mockResolvedValue(fakeImages);
+    twitter.uploadImage = await jest.fn().mockResolvedValue(uploadImageAPIResponse);
+    console.error = jest.fn();
+
+    // test and assert
+    expect.assertions(2);
+    await expect(index.handler).rejects.toThrow();
+    expect(console.error).toHaveBeenCalledWith('Twitter error details:', {});
+  })
+})
